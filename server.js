ddbabf23dbfa00aa299afe2a167688c1c9bb9f4e
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-07-19 14:31:17 -0700

    server.js updated with controllers

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
+// Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const mongoUri =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/tipsytiki";
-mongoose.connect(
-  mongoUri,
-  { useNewUrlParser: true }
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// Drinks Controller
+const DrinksController = require('./controllers/drinks.js')
+app.use('/drinks', DrinksController)
+
+// Sessions Controller
+const SessionsController = require('./controllers/sessions.js')
+app.use('/sessions', SessionsController)
+
+// Users controller
+const UsersController = require('./controllers/users.js')
+app.use('/users', UsersController)
+
+
+// Mongoose Connection
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/tipsytiki";
+mongoose.connect( mongoUri, { useNewUrlParser: true }
 );
 
+// Port
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
