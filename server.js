b0bc2e8dc3a2682ec23b50b8156b268fc1e78540
colleencleary <cleary.ce@gmail.com>
2018-07-19 15:04:16 -0400

    updating for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/tipsytiki';
+ mongoose.connect(mongoUri, { useNewUrlParser: true });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+  console.log('listening');
+});
