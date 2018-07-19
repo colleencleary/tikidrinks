e3f851cbb4532f835927d38699b7b128a3dd70d7
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-07-19 17:33:31 -0400

    )

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -1,33 +1,38 @@
-const express = require('express')
-const router = express.Router()
-const Drink = require('../models/drinksModel.js')
+const express = require("express");
+const router = express.Router();
+const Drink = require("../models/drinksModel.js");
 
 // Index Route
-router.get('/', (req, res)=>{
-  Drink.find({}, (error, Drinks) =>{
-    res.json(Drinks)
-  })
-})
+router.get("/", (req, res) => {
+  Drink.find({}, (error, Drinks) => {
+    res.json(Drinks);
+  });
+});
 
 // Create Route
-router.post('/', (req, res)=>{
-  Drink.create(req.body, (error, createdDrink)=>{
-    res.json(createdDrink)
-  })
-})
+router.post("/", (req, res) => {
+  Drink.create(req.body, (error, createdDrink) => {
+    res.json(createdDrink);
+  });
+});
 
 // Delete Route
-router.delete('/:id', (req, res)=>{
-  Drink.findByIdAndRemove(req.params.id, (error, deletedDrink)=>{
-    res.json(deletedDrink)
-  })
-})
+router.delete("/:id", (req, res) => {
+  Drink.findByIdAndRemove(req.params.id, (error, deletedDrink) => {
+    res.json(deletedDrink);
+  });
+});
 
 // Update Route
-router.put('/:id', (req, res)=>{
-  Drink.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedDrink)=>{
-    res.json(updatedDrink
-  })
-})
+router.put("/:id", (req, res) => {
+  Drink.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (error, updatedDrink) => {
+      res.json(updatedDrink);
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
